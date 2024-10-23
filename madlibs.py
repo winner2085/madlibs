a498665be1216a45ac42a6f06c8c818f3e35aba8
Erica <ericac2700@tuckahoeschools.org>
2024-10-23 12:25:13 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,34 @@
+adjective1 = input("adjective: ")
+object1 = input("object: ")
+name1 = input("name: ")
+drink1 = input("drink: ")
+name2 = input("name: ")
+adjective2 = input("adjective: ")
+animal1 = input("animal: ")
+adjective3 = input("adjective: ")
+noun1 = input("noun: ")
+verb1 = input("verb:")
+adjective4 = ("adjective: ")
+verb2 = ("verb: ")
+sound1 = ("sound: ")
+feeling1 = ("feeling: ")
+adjective5 =  input("adjective: ")
+adjective6 = input("adjective: ")
+adverb1 = input("adverb: ")
+name3: = input("name: ")
+transportation1 = input("mode of transportation: ")
+pluralnoun1 = input("plural noun: ")
+
+print(f"""This is the beginning where a {adjective1} {object1} lies in {name1}'s backyard.
+    He watches the object with a {drink1} cup in his hand, wondering how got here.
+    He thinks of {name2}, his neigbor, and their {adjective2} {animal1} that likes to sneak into his yard.
+    But the object was too {adjective3} for a pet to bring in, so he examines the sky, a long-shot, but as he was looking, he noticed a {noun1} against the blue.
+    {name1} {verb1} to try and get a better look and sees as the {noun1} is getting closer, and it was {adjective4}.
+    He {verb2} as fast as he could back to the house and as he was reaching for the door, he heard a {sound1} from behind him and the whole ground shook.
+    {name1} didn't want to look back, but if there was something in his backyard, he was {feeling1}.
+    He turned around and saw the {noun1}, {adjective5} and {adjective6}.
+    He heard his wife calling out for him as he {adverb1} got closer to the {noun1}.
+    {name3} was yelling and telling him to go to the {transportation1} while she grabbed the {pluralnoun1}, but {name1} couldn't stop staring at it.
+    Why was a {noun1} in his backyard and what did it have to do with a {adjective1} {object1}?
+    Only when he looked at the sky again because of a sound, did he realize what was happening.
+""")
